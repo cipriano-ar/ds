8a785a8b0e925d14f1ea41f8c8820f51ebbf33c3
cmazzieri <cmazzieri@gmail.com>
2015-02-05 08:49:55 +1300

    files updated

--- js/main.js
+++ js/main.js
@@ -4,14 +4,16 @@ var map;
 var hidden;
 function init() {
 	
+	// Map Options
 	var mapOptions = {
 		center: {lat:-34.397,lng:150.644},
 		zoom: 11,
 		scrollwheel:false,
 		marker: {lat:-34.397,lng:150.644}
 	};
+
+	// Adds the Map with the options
 	var map_container = document.getElementById('map');
-	// Adds the Map with basic options
 	map = new google.maps.Map(map_container, mapOptions);
 
 	// Adds a marker to the map
@@ -20,23 +22,20 @@ function init() {
 		position: {lat:-34.397,lng:150.644}
 	});
 
-	// Create an Info Window and set content for it
+	// Creates an Info Window and set content for it
 	var info_window = new google.maps.InfoWindow();
 	info_window.setContent('<b>Hi Bro!</b>');
 
-	// Adds "click" event to the marker and show Info Window
+	// Adds "click" event to the marker and shows Info Window
 	marker.addListener('click', function() {
 		info_window.open(map,this);
 	});
 
-	// Swap visibility of the marker on the map
-	// var marker_btn = document.getElementsByClassName('marker-visib');
-	// marker_btn.addListener('click', function() {
-		
-	// });
-
-	$(menuSlide('.days-menu', '.days-menu-items',500));
-	$(menuSlide('.opts-menu', '.options-menu-items',500));
+	// Swaps visibility of the marker on the map
+	
+	// Slide Menus set up
+	$(menuSlide('.days-btn', '.days-menu-items',300));
+	$(menuSlide('.options-btn', '.options-menu-items',300));
 
 }
 
