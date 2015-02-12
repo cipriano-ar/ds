903e1b78b664cdc064c034613e97cecc965412aa
Cipriano Mazzieri <cipriano.mazzieri@43374-501.auckland.natcoll.ac.nz>
2015-02-12 19:49:04 +1300

    12/02/2015 19:55

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ init();
 function init() {
 	
 	// Map Options
-	var mapOptions = {
+	var map_options = {
 		center: {lat:-36.8495452,lng:174.7669572},
 		zoom: 15,
 		scrollwheel:false
@@ -13,30 +13,33 @@ function init() {
 
 	// Adds the Map with the options
 	var map_container = document.getElementById('map');
-	map = new google.maps.Map(map_container, mapOptions);
+	map = new google.maps.Map(map_container, map_options);
 
 	// Adds a marker to the map
 	var marker = new google.maps.Marker({
-		map: map,
-		position: {lat:-36.8495452,lng:174.7669572}
+		icon: './media/images/markers/food-marker.png',
+		position: {lat:-36.8495452,lng:174.7669572},
+		map: map
 	});
 
 	// Creates an Info Window and set content for it
 	var info_window = new google.maps.InfoWindow();
 	info_window.setContent('<b>Hi Bro!</b>');
 
+	map.data.loadGeoJson('markers.json');
+
 	/* ++++++++++++++ LISTENERS ++++++++++++++ */
+
+	// Adds "click" event to the marker and shows Info Window
+	marker.addListener('click', function() {
+		info_window.open(map,this);
+	});
 
 	// keeps map centered on resize (responsive)
 	google.maps.event.addDomListener(window,'resize', function(){
 		var center = map.getCenter();
-		google.maps.event.trigger(map, 'resize');
+		google.maps.event.trigger(map,'resize');
 		map.setCenter(center);
-	});
-
-	// 'click' and shows Info Window
-	marker.addListener('click', function() {
-		info_window.open(map,this);
 	});
 	
 	// Slide Menus set up
@@ -50,3 +53,8 @@ function menuSlide(button, navigation, slide_duration) {
 		$(navigation).toggle(slide_duration);
 	});
 }
+
+// Adds markers. 'coordinates' is an array of "{lat: ,long: }" objects and 'categories' is an array of strings with any of this values: bar, event, food
+// function addMarkers(coordinates, icons) {
+
+// }
